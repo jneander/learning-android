816abf36d6d83988548037e7743776e2cf915380
Jeremy Neander <subjectego@gmail.com>
2014-01-12 16:16:35 -0600

    Add logging

--- GeoQuiz/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,10 +26,14 @@ public class QuizActivity extends ActionBarActivity {
 
   private int mCurrentIndex = 0;
 
+  private static final String TAG = "QuizActivity";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_quiz);
+
+    Log.d(TAG, "onCreate(Bundle) called");
 
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
@@ -76,6 +81,36 @@ public class QuizActivity extends ActionBarActivity {
     }
 
     Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume() called");
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
   }
 
   @Override
