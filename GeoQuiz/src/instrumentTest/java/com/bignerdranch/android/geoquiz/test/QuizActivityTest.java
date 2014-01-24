a2f70efa56e28d56d230ece0b1c945e15c40d9a2
Jeremy Neander <subjectego@gmail.com>
2014-01-24 16:11:44 -0600

    Add failing example test

---
+++ GeoQuiz/src/instrumentTest/java/com/bignerdranch/android/geoquiz/test/QuizActivityTest.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.geoquiz.test;
+
+import junit.framework.TestCase;
+
+public class QuizActivityTest extends TestCase {
+  public void testFailure() {
+    assertEquals(true, false);
+  }
+}
